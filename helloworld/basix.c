5093a5b8b6237e5df5881ce2fb91ee72040f8aaa
Tejas gowda.S <tgowda934@gmail.com>
2024-11-03 12:24:27 +0530

    third commit

---
+++ helloworld/basix.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    int input_no,b, i,sum;
+    
+
+    for(i=0;i<5;i++){ 
+      
+           if (input_no>50) break;
+    printf("enter here %d th time here :", b++);
+    scanf("%d",&input_no);
+    if (input_no<0) continue ;
+     sum+=input_no;
+    printf("the result is %d\n",sum);
+   
+  
+   
+}}
